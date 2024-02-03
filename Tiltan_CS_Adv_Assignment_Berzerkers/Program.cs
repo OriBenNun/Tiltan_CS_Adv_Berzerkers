99abead9a10753ab3585d19c1be9c41846af9336
BmanStudio <57292081+BmanStudio@users.noreply.github.com>
2024-02-03 18:49:20 +0200

    Added tests and organized the code for easier use

--- Tiltan_CS_Adv_Assignment_Berzerkers/Program.cs
+++ Tiltan_CS_Adv_Assignment_Berzerkers/Program.cs
@@ -11,25 +11,89 @@ namespace Tiltan_CS_Adv_Assignment_Berzerkers
 
         private static void Test()
         {
+            DiceTest();
 
+            // UnitAttackTest();
+        }
+
+        private static void DiceTest()
+        {
+            DiceEqualityTest();
+            
+            // SingleDieTest();
+        }
+
+        private static void DiceEqualityTest()
+        {
             var d1 = new Dice(2, 8, 1);
             var d2 = new Dice(2, 8, 1);
-            
-            Console.WriteLine(d1.ToString());
-            Console.WriteLine(d2.ToString());
-            Console.WriteLine(d1.Equals(d2));
-            Console.WriteLine(d1.GetHashCode());
-            Console.WriteLine(d2.GetHashCode());
-            Console.WriteLine(d1.GetHashCode() == d2.GetHashCode());
+            var d3 = new Dice(4, 6, 4);
 
-            // var s = new Tank(name: "Aba");
-            // var p = new Guardian(name: "Gobo");
-            // var pp = new Guardian(name: "Loooko");
-            //
-            // Console.WriteLine(s);
-            // Console.WriteLine(p);
-            // Console.WriteLine(pp);
-            //
+            Console.WriteLine(d1.Equals(d2));
+            Console.WriteLine(d1.Equals(d3));
+            Console.WriteLine(d2.GetHashCode() == d3.GetHashCode());
+
+            Console.WriteLine($"D1 rolled: {d1.Roll()}");
+            Console.WriteLine($"D2 rolled: {d2.Roll()}");
+            Console.WriteLine($"D3 rolled: {d3.Roll()}");
+            
+        }
+
+        private static void SingleDieTest()
+        {
+            var d1 = new Dice(1, 6, 0);
+            var ones = 0;
+            var twos = 0;
+            var threes = 0;
+            var fours = 0;
+            var fives = 0;
+            var sixes = 0;
+
+            for (int i = 0; i < 100_000; i++)
+            {
+                var roll = d1.Roll();
+
+                switch (roll)
+                {
+                    case 1:
+                        ones++;
+                        break;
+                    case 2:
+                        twos++;
+                        break;
+                    case 3:
+                        threes++;
+                        break;
+                    case 4:
+                        fours++;
+                        break;
+                    case 5:
+                        fives++;
+                        break;
+                    case 6:
+                        sixes++;
+                        break;
+                }
+            }
+
+            Console.WriteLine(ones);
+            Console.WriteLine(twos);
+            Console.WriteLine(threes);
+            Console.WriteLine(fours);
+            Console.WriteLine(fives);
+            Console.WriteLine(sixes);
+        }
+
+        private void UnitAttackTest()
+        {
+            var s = new Tank(name: "Aba");
+            var p = new Guardian(name: "Gobo");
+            var pp = new Guardian(name: "Loooko");
+
+            Console.WriteLine(s);
+            Console.WriteLine(p);
+            Console.WriteLine(pp);
+
             // s.Attack(p);
             // p.Attack(s);
             // s.Attack(p);
@@ -97,10 +161,10 @@ namespace Tiltan_CS_Adv_Assignment_Berzerkers
             // pp.Attack(s);
             // pp.Attack(s);
             // pp.Attack(s);
-            //
-            // Console.WriteLine(s);
-            // Console.WriteLine(p);
-            // Console.WriteLine(pp);
+
+            Console.WriteLine(s);
+            Console.WriteLine(p);
+            Console.WriteLine(pp);
         }
     }
 }
