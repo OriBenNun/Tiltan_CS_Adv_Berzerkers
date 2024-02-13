4f1be2c879b6eb95fa8a8d0bce430358f367acf9
BmanStudio <57292081+BmanStudio@users.noreply.github.com>
2024-02-14 01:11:42 +0200

    Added the logic for the end of war (WIP)

--- Tiltan_CS_Adv_Assignment_Berzerkers/GameManager.cs
+++ Tiltan_CS_Adv_Assignment_Berzerkers/GameManager.cs
@@ -11,23 +11,73 @@ public class GameManager
 
     public void PlayersWar(Player player1, Player player2)
     {
-        Console.WriteLine($"Two brave players have showed up for this upcoming war!\n\n" +
+        Console.WriteLine($"Two Brave Players Have Showed Up For An Unbelievable War! May The Best Win!\n\n" +
                           $"Player #1 Information:\n" +
                           $"{player1}\n\n" +
                           $"Player #2 Information:\n" +
                           $"{player2}\n\n" +
                           $"LET THE WAR BEGIN!!");
+
+        var fightResult = UnitsFight(player1.Units, player2.Units);
+
+        var message = "";
+        switch (fightResult)
+        {
+            case FightResult.Draw:
+                message = $"No resources were stolen, as no units left alive.\n";
+                break;
+            case FightResult.TeamAWon:
+                MoveResourcesFromLoserToWinner(player1, player2);
+                break;
+            case FightResult.TeamBWon:
+                MoveResourcesFromLoserToWinner(player2, player1);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+
+        message += $"Player #1 Information:\n" +
+                   $"{player1}\n\n" +
+                   $"Player #2 Information:\n" +
+                   $"{player2}\n\n" +
+                   $"THE WAR IS OVER FOR TODAY. LET THE TROOPS REST UNTIL NEXT TIME!";
         
-        UnitsFight(player1.Units, player2.Units);
+        Console.WriteLine(message);
     }
 
-    private void UnitsFight(List<Unit> teamA, List<Unit> teamB)
+    private void MoveResourcesFromLoserToWinner(Player winner, Player loser)
     {
-        Console.WriteLine($"-------THE FIGHT HAS STARTED-------");
+        var winnerAliveUnits = Unit.GetAliveUnitsList(winner.Units);
+        foreach (var aliveUnit in winnerAliveUnits)
+        {
+            var amountToSteal = RandomChanceChecker.GetRandomInteger(aliveUnit.Capacity + 1, 1);
+            var stolenResources = loser.StealResources((uint)amountToSteal, $"{aliveUnit.UnitName} [{winner.Name}]");
+
+            // Means the loser has no resources left
+            if (stolenResources == 0)
+            {
+                break;
+            }
+
+            winner.GainResources(stolenResources);
+        }
+    }
+
+    /// <summary>
+    /// The algorithm for the actual units fight, which ends when one (or both) of the teams has no units left.
+    /// Note that at every step of the algo, a unit from (<paramref name="teamA"/>) will try to attack first a unit from (<paramref name="teamB"/>),
+    /// so there is some significant to the order by which the lists are being passed. 
+    /// </summary>
+    /// <param name="teamA">List of Unit objects that represents the first team</param>
+    /// <param name="teamB">List of Unit objects that represents the second team</param>
+    /// <returns>The result of the units fight</returns>
+    private FightResult UnitsFight(List<Unit> teamA, List<Unit> teamB)
+    {
+        Console.WriteLine($"-------THE UNITS FIGHT HAS STARTED-------");
         var roundCounter = 1;
-        var liveUnitsCountTeamA = GetAliveUnitsCount(teamA);
-        var liveUnitsCountTeamB = GetAliveUnitsCount(teamB);
-        
+        var liveUnitsCountTeamA = Unit.GetAliveUnitsCount(teamA);
+        var liveUnitsCountTeamB = Unit.GetAliveUnitsCount(teamB);
+
         while (liveUnitsCountTeamA > 0 && liveUnitsCountTeamB > 0)
         {
             Console.WriteLine($"\n\nRound #{roundCounter} started.\n\n" +
@@ -45,12 +95,12 @@ public class GameManager
             _currentWeatherRoundsCounter--;
 
             RoundFight(teamA, teamB);
-            
-            liveUnitsCountTeamA = GetAliveUnitsCount(teamA);
-            liveUnitsCountTeamB = GetAliveUnitsCount(teamB);
+
+            liveUnitsCountTeamA = Unit.GetAliveUnitsCount(teamA);
+            liveUnitsCountTeamB = Unit.GetAliveUnitsCount(teamB);
             roundCounter++;
         }
-        
+
         // Handling the edge case where the 2 teams has no live units left (most likely because of a weather event)
         if (liveUnitsCountTeamA == 0 && liveUnitsCountTeamB == 0)
         {
@@ -58,84 +108,40 @@ public class GameManager
                               $"The two teams has no live unit left!\n" +
                               $"Therefore, the fight ended with no winner. Only losers.\n" +
                               $"-------THE FIGHT IS OVER-------");
-            return;
+            return FightResult.Draw;
         }
 
         var isTeamAWinner = liveUnitsCountTeamA > 0;
-        var aliveUnitsWinnerTeam = GetAliveUnitsList(isTeamAWinner ? teamA : teamB);
 
         var winnerString = isTeamAWinner ? "Team A" : "Team B";
         var winnerTeamAliveCount = isTeamAWinner ? liveUnitsCountTeamA : liveUnitsCountTeamB;
-        // TODO add stealing loot according to the loser resources, and the capacity of the winner's alive units (each steals a random amount if the loser has resources left)
         
         Console.WriteLine($"\n\nThe fight is over at round #{roundCounter}!\n" +
                           $"The winner is: {winnerString}!!\n" +
                           $"They won having {winnerTeamAliveCount} alive unit[s] left.\n" +
-                          $"-------THE FIGHT IS OVER-------");
+                          $"-------THE UNITS FIGHT IS OVER-------");
+
+        return isTeamAWinner ? FightResult.TeamAWon : FightResult.TeamBWon;
     }
 
     private void RoundFight(List<Unit> teamA, List<Unit> teamB)
     {
-        if (GetAliveUnitsCount(teamA) == 0 || GetAliveUnitsCount(teamB) == 0)
+        if (Unit.GetAliveUnitsCount(teamA) == 0 || Unit.GetAliveUnitsCount(teamB) == 0)
         {
             Console.WriteLine($"One of the teams has no units alive! aborting the round's fight!");
             return;
         }
-        
-        var aliveUnitsTeamA = GetAliveUnitsList(teamA);
-        var aliveUnitsTeamB = GetAliveUnitsList(teamB);
+
+        var aliveUnitsTeamA = Unit.GetAliveUnitsList(teamA);
+        var aliveUnitsTeamB = Unit.GetAliveUnitsList(teamB);
 
         var randomUnitTeamA = aliveUnitsTeamA[RandomChanceChecker.GetRandomInteger(aliveUnitsTeamA.Count)];
         var randomUnitTeamB = aliveUnitsTeamB[RandomChanceChecker.GetRandomInteger(aliveUnitsTeamB.Count)];
-        
+
         randomUnitTeamA.Fight(randomUnitTeamB);
         randomUnitTeamB.Fight(randomUnitTeamA);
     }
     
-    private List<Unit> GetAliveUnitsList(List<Unit> units)
-    {
-        if (units.Count == 0)
-        {
-            return null;
-        }
-        
-        var aliveUnits = new List<Unit>();
-
-        foreach (var unit in units)
-        {
-            if (unit.GetIsDead())
-            {
-                continue;
-            }
-
-            aliveUnits.Add(unit);
-        }
-
-        return aliveUnits;
-    }
-
-    private int GetAliveUnitsCount(List<Unit> units)
-    {
-        if (units.Count == 0)
-        {
-            return 0;
-        }
-        
-        var counter = 0;
-
-        foreach (var unit in units)
-        {
-            if (unit.GetIsDead())
-            {
-                continue;
-            }
-
-            counter++;
-        }
-
-        return counter;
-    }
-
     private static void ApplyWeatherEffectToUnitList(List<Unit> teamA, Weather newWeather)
     {
         teamA.ForEach((unit) => unit.WeatherEffect(newWeather));
@@ -152,5 +158,12 @@ public class GameManager
         var randomWeather = RandomChanceChecker.GetRandomInteger(
             Enum.GetValues(typeof(Weather)).Length, 1);
         return (Weather)randomWeather;
+    }
+
+    private enum FightResult
+    {
+        Draw,
+        TeamAWon,
+        TeamBWon,
     }
 }
