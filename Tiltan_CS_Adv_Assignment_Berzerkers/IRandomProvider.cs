9a8f8ed63dd2f12627374a5d7c13c48eda7774ab
BmanStudio <57292081+BmanStudio@users.noreply.github.com>
2024-02-15 15:19:24 +0200

    Added the bag struct as required in the assignment (didnt test it yet)

--- Tiltan_CS_Adv_Assignment_Berzerkers/IRandomProvider.cs
+++ Tiltan_CS_Adv_Assignment_Berzerkers/IRandomProvider.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Tiltan_CS_Adv_Assignment_Berzerkers;
 
@@ -19,7 +20,7 @@ public readonly struct Dice : IEquatable<Dice>, IRandomProvider
         BaseDie = baseDie;
         Modifier = modifier;
     }
-    
+
     public int GetRandom(string unitName = "[Unknown Unit]")
     {
         return Roll(unitName);
@@ -62,18 +63,86 @@ public readonly struct Dice : IEquatable<Dice>, IRandomProvider
 
         return $"{Scalar}d{BaseDie} {suffix}";
     }
-    
+
     private int Roll(string unitName)
     {
         var result = 0;
-        
+
         for (var i = 0; i < Scalar; i++)
         {
             var rollResult = RandomChanceChecker.GetRandomInteger((int)BaseDie + 1, 1);
             result += rollResult;
         }
-        
+
         Console.WriteLine($"[{unitName}] rolled: {result + Modifier}");
         return result + Modifier;
+    }
+}
+
+public readonly struct Bag : IRandomProvider
+{
+    private uint BiggestNumber { get; }
+    private uint SmallestNumber { get; }
+
+    private readonly List<uint> _currentBag;
+
+    public Bag(uint biggestNumber, uint smallestNumber)
+    {
+        if (biggestNumber <= smallestNumber)
+        {
+            Console.WriteLine(
+                $"ATTENTION!! Bag struct was initialized with biggest number that isn't bigger than the smallest number.\n" +
+                $"Changing the biggest number value to be higher by 1 than the smallest number, so the bag functions properly.");
+            biggestNumber = smallestNumber + 1;
+        }
+
+        BiggestNumber = biggestNumber;
+        SmallestNumber = smallestNumber;
+
+        _currentBag = new List<uint>();
+
+        ResetBag();
+    }
+
+    private void ResetBag()
+    {
+        _currentBag.Clear();
+
+        var possibleOutcomes = new List<uint>();
+
+        for (uint i = SmallestNumber; i <= BiggestNumber; i++)
+        {
+            possibleOutcomes.Add(i);
+        }
+
+        for (var i = 0; i < possibleOutcomes.Count; i++)
+        {
+            var randomIndex = RandomChanceChecker.GetRandomInteger(possibleOutcomes.Count);
+            var randomElementToAdd = possibleOutcomes[randomIndex];
+            possibleOutcomes.RemoveAt(randomIndex);
+            _currentBag.Add(randomElementToAdd);
+        }
+        
+        Console.WriteLine($"Bag was reset and reshuffled");
+    }
+
+    public int GetRandom(string unitName = "[Unknown Unit]")
+    {
+        return (int)PickFromTopOfBag(unitName);
+    }
+
+    private uint PickFromTopOfBag(string unitName)
+    {
+        if (_currentBag.Count == 0)
+        {
+            ResetBag();
+        }
+        
+        var result = _currentBag[0];
+        _currentBag.RemoveAt(0);
+        
+        Console.WriteLine($"[{unitName}] picked from bag: {result}");
+
+        return result;
     }
 }
